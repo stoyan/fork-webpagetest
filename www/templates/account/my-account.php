24d526b52a3e17865348f870d8dd59fbf2f72ed7
Sue Lockwood <deathbearbrown@gmail.com>
2022-06-16 10:51:20 -0700

    Restructure data on page

--- www/templates/account/my-account.php
+++ www/templates/account/my-account.php
@@ -1,70 +1,185 @@
 <div class="my-account-page page_content">
-  <?php if (!$is_verified): ?>
-  <div class="resend-email-verification-container">
-    <div class="resend-email-verification-hed">
-      <h3>A verification link was sent to your email</h3>
-    </div>
-    <div>Please click on the link that was sent to your email to complete your registration process.</div>
-    <div class="resend-link-container">
-      <span>Didn’t receive an email?</span>
-      <form method="POST" action="/account">
-        <input type="hidden" name="type" value="resend-verification-email" />
-        <input type="hidden" name="csrf_token" value="<?= $csrf_token ?>" />
-        <button type="submit">Resend Verification Link</button>
-      </form>
-    </div>
-  </div>
-  <?php endif; ?>
-
-  <div class="subhed">
-    <h1>My Account</h1>
-    <?php if ($is_paid): ?>
-    <div class="contact-support-button">
-      <a href="https://support.webpagetest.org"><span>Contact Support</span></a>
-    </div>
+    <!-- VERIFIED EMAIL NOTICE ---->
+    <?php if (!$is_verified) : ?>
+        <div class="resend-email-verification-container">
+            <div class="resend-email-verification-hed">
+                <h3>A verification link was sent to your email</h3>
+            </div>
+            <div>Please click on the link that was sent to your email to complete your registration process.</div>
+            <div class="resend-link-container">
+                <span>Didn’t receive an email?</span>
+                <form method="POST" action="/account">
+                    <input type="hidden" name="type" value="resend-verification-email" />
+                    <input type="hidden" name="csrf_token" value="<?= $csrf_token ?>" />
+                    <button type="submit">Resend Verification Link</button>
+                </form>
+            </div>
+        </div>
     <?php endif; ?>
-  </div>
 
-  <div class="box card contact-info" data-modal="contact-info-modal">
-    <div class="card-section user-info">
-    <span class="dot"><?= htmlspecialchars($first_name)[0] . ' ' . htmlspecialchars($last_name)[0] ?> </span>
-      <div>
-      <h3><?= htmlspecialchars($first_name) . ' ' . htmlspecialchars($last_name) ?></h3>
-      <div class="info">
-        <div><?= htmlspecialchars($email) ?></div>
-      </div>
-    </div>
-    </div>
-    <div class="card-section">
-      <div class="edit-button">
-        <button><span>Edit</span></button>
-      </div>
-    </div>
-  </div>
 
-  <div class="box card password" data-modal="password-modal">
-    <div class="card-section">
-      <h3>Password</h3>
-      <div class="info">
-        <div>************</div>
-      </div>
+    <!-- Page Subheader -->
+    <div class="subhed">
+        <h1>My Account</h1>
+        <?php if ($is_paid) : ?>
+            <div class="contact-support-button">
+                <a href="https://support.webpagetest.org"><span>Contact Support</span></a>
+            </div>
+        <?php endif; ?>
     </div>
-    <div class="card-section">
-      <div class="edit-button">
-        <button><span>Edit</span></button>
-      </div>
-    </div>
-  </div>
 
-<?php if ($is_paid) {
-  include_once __DIR__ . '/includes/billing-data.php';
-  include_once __DIR__ . '/includes/modals/subscription-plan.php';
-  include_once __DIR__ . '/includes/modals/payment-info.php';
-} else {
-  include_once __DIR__ . '/includes/signup.php';
-} ?>
+    <!-- account tabs for Settings, Invoices and APIs -->
+    <div class="tabs-container">
+        <!-- radio buttons control the JS-less tabs-->
+        <input type="radio" name="account-tabs" id="account-settings" value="account settings" checked />
+        <!-- these sections only exist for paid users-->
+        <?php if ($is_paid) : ?>
+            <input type="radio" name="account-tabs" id="payments-invoices" value="payments and invoices" />
+            <input type="radio" name="account-tabs" id="api-consumers" value="api consumers" />
+        <?php endif; ?>
+
+        <!-- these link to sections on the account page, but have subpages for modifications,  use hash deep linking -->
+        <div class="tab-labels">
+            <label for="account-settings">Account Settings</label>
+            <!-- these sections only exist for paid users-->
+            <?php if ($is_paid) : ?>
+                <label for="payments-invoices">Payments and Invoices</label>
+                <label for="api-consumers">Api Consumers</label>
+            <?php endif; ?>
+        </div>
+
+        <!-- account settings tab -->
+        <div class="tab-content">
+            <div class="box card contact-info" data-modal="contact-info-modal">
+                <div class="card-section user-info">
+                    <span class="dot"><?= htmlspecialchars($first_name)[0] . ' ' . htmlspecialchars($last_name)[0] ?> </span>
+                    <div>
+                        <h3><?= htmlspecialchars($first_name) . ' ' . htmlspecialchars($last_name) ?></h3>
+                        <div class="info">
+                            <div><?= htmlspecialchars($email) ?></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-section">
+                    <div class="edit-button">
+                        <button><span>Edit</span></button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="box card password" data-modal="password-modal">
+                <div class="card-section">
+                    <h3>Password</h3>
+                    <div class="info">
+                        <div>************</div>
+                    </div>
+                </div>
+                <div class="card-section">
+                    <div class="edit-button">
+                        <button><span>Edit</span></button>
+                    </div>
+                </div>
+            </div>
+            <?php if ($is_paid) {
+                include_once __DIR__ . '/includes/modals/subscription-plan.php';
+            } ?>
+        </div>
+
+        <!-- Billing Invoice tab -->
+        <div class="tab-content">
+            <?php if ($is_paid) {
+                include_once __DIR__ . '/includes/billing-data.php';
+                include_once __DIR__ . '/includes/modals/payment-info.php';
+            } else {
+                include_once __DIR__ . '/includes/signup.php';
+            } ?>
+        </div>
+
+        <!-- API tab -->
+        <div class="tab-content">
+            <div class="box card api-consumers">
+                <h3>API Consumers</h3>
+                <div class="create-key-container">
+                    <div class="create-delete-button-container">
+                        <button type="button" class="new-api-key" data-toggle="open" data-targetid="create-api-key-toggle-area">New Api Key</button>
+                        <label for="delete-api-key-submit-input" class="delete-key disabled" data-apikeybox="delete-button" disabled>Delete</label>
+                    </div>
+                    <div class="toggleable" id="create-api-key-toggle-area">
+                        <form method="POST" action="/account">
+                            <label for="api-key-name" class="sr-only">API Key Name</label>
+                            <input type="text" name="api-key-name" placeholder="Enter Application Name" required />
+                            <button type="submit">Save</button>
+                            <button type="button" class="cancel" data-toggle="close" data-targetid="create-api-key-toggle-area">Cancel</button>
+                            <input type="hidden" name="type" value="create-api-key" />
+                            <input type="hidden" name="csrf_token" value="<?= $csrf_token ?>" />
+                        </form>
+                    </div>
+                </div>
+                <div class="info">
+                    <form method='POST' action='/account'>
+                        <table class="sortable">
+                            <caption>
+                                <span class="sr-only">API Consumers table, column headers with buttons are sortable.</span>
+                            </caption>
+                            <thead>
+                                <tr>
+                                    <th class="no-sort select-all-box"><label class="sr-only" for="select-all-api-keys">Select all api keys</label><input type="checkbox" name="select-all-api-keys" data-apikeybox="select-all" /></th>
+                                    <th aria-sort="ascending">
+                                        <button>
+                                            Name
+                                            <span aria-hidden="true"></span>
+                                        </button>
+                                    </th>
+                                    <th>
+                                        <button>
+                                            API Key
+                                            <span aria-hidden="true"></span>
+                                        </button>
+                                    </th>
+                                    <th>
+                                        <button>
+                                            Create Date
+                                            <span aria-hidden="true"></span>
+                                        </button>
+                                    </th>
+                                    <th>
+                                        <button>
+                                            Last Updated
+                                            <span aria-hidden="true"></span>
+                                        </button>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($wptApiKey as $row) : ?>
+                                    <tr>
+                                        <td>
+                                            <input type='checkbox' data-apikeybox="individual" name='api-key-id[]' value='<?= $row['id'] ?>' />
+                                        </td>
+                                        <td><?= $row['name'] ?></td>
+                                        <td class="hidden-content">
+                                            <button type="button" class="view-button">View</button>
+                                            <span class="hidden-area closed">
+                                                <span class="api-key"><?= $row['apiKey'] ?></span>
+                                                <button type="button" class="hide-button"><span class="sr-only">Close</span></button>
+                                        </td>
+                                        </span>
+                                        <td><?= date_format(date_create($row['createDate']), 'M d Y H:i:s e') ?></td>
+                                        <td><?= date_format(date_create($row['changeDate']), 'M d Y H:i:s e') ?></td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                        <input type='hidden' name='type' value='delete-api-key' />
+                        <input type='hidden' name='csrf_token' value='<?= $csrf_token ?>' />
+                        <input type="submit" id="delete-api-key-submit-input" class="sr-only" />
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </div>
-
 
 <!-- Modals -->
 <?php
