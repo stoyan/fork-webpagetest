1bfb7f29cb87619d5c65ff39cf8758b42d6226b6
Sue Lockwood <deathbearbrown@gmail.com>
2022-06-17 10:23:42 -0700

    move around payments and invoices

--- www/templates/account/my-account.php
+++ www/templates/account/my-account.php
@@ -121,7 +121,7 @@
         <?php if ($is_paid) : ?>
             <div class="tab-content" id="billing-settings-content">
                 <?php if ($is_paid) {
-                    include_once __DIR__ . '/includes/billing-data.php';
+                    include_once __DIR__ . '/includes/billing/invoice-history.php';
                 } else {
                     include_once __DIR__ . '/includes/signup.php';
                 } ?>
